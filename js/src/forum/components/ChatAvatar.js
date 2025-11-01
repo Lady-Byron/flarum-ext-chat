d8d9bea4f98351cbbc01313888a6e3c05f3aed64
Lady-Byron <samantha-everett@outlook.com>
2025-11-01 18:24:58 -0400

    Update ChatAvatar.js

--- js/src/forum/components/ChatAvatar.js
+++ js/src/forum/components/ChatAvatar.js
@@ -1,8 +1,6 @@
 // js/src/forum/components/ChatAvatar.js
-// [CHANGED] Imports to flarum/common/*; class -> className; firstLetter 空值守护
-
-import Component from 'flarum/common/Component';            // [CHANGED]
-import classList from 'flarum/common/utils/classList';       // [CHANGED]
+import Component from 'flarum/common/Component';
+import classList from 'flarum/common/utils/classList';
 
 export default class ChatAvatar extends Component {
   oninit(vnode) {
@@ -16,43 +14,47 @@ export default class ChatAvatar extends Component {
       <div
         className={classList({ avatar: true, image: !!avatar })}
         style={{
-          'background-color': this.model.color && this.model.color(),
+          backgroundColor: this.model.color && this.model.color(),
           color: this.model.textColor && this.model.textColor(),
-          'background-image': avatar ? `url(${avatar})` : null,
+          backgroundImage: avatar ? `url(${avatar})` : undefined,
         }}
       >
         {this.model.icon && this.model.icon() ? (
-          <i className={this.model.icon()}></i>  {/* [CHANGED] */}
+          <i className={this.model.icon()}></i>
         ) : avatar ? null : (
-          this.firstLetter((this.model.title && this.model.title()) || '').toUpperCase()
+          (this.firstLetter((this.model.title && this.model.title()) || '') || '').toUpperCase()
         )}
       </div>
     );
   }
 
   componentAvatarChannel() {
+    const avatar = this.model.avatarUrl && this.model.avatarUrl();
     return (
       <div
         className="avatar"
         style={{
-          'background-color': this.model.color && this.model.color(),
+          backgroundColor: this.model.color && this.model.color(),
           color: this.model.textColor && this.model.textColor(),
+          backgroundImage: avatar ? `url(${avatar})` : undefined,
         }}
       >
         {this.model.icon && this.model.icon() ? (
-          <i className={this.model.icon()}></i>  {/* [CHANGED] */}
-        ) : this.model.avatarUrl && this.model.avatarUrl() ? null : (
-          this.firstLetter((this.model.title && this.model.title()) || '').toUpperCase()
+          <i className={this.model.icon()}></i>
+        ) : avatar ? null : (
+          (this.firstLetter((this.model.title && this.model.title()) || '') || '').toUpperCase()
         )}
       </div>
     );
   }
 
   view() {
-    return (this.model.type && this.model.type()) == 1 ? this.componentAvatarChannel() : this.componentAvatarPM();
+    return (this.model.type && this.model.type()) == 1
+      ? this.componentAvatarChannel()
+      : this.componentAvatarPM();
   }
 
-  firstLetter(str) {                        // [CHANGED] null guard
+  firstLetter(str) {
     if (!str) return '';
     for (let i = 0; i < str.length; i++) {
       if (this.isLetter(str[i])) return str[i];
