7cd21331778d83c1882868372ba850637139b173
Lady-Byron <samantha-everett@outlook.com>
2025-11-01 00:45:14 -0400

    Update ChatSerializer.php

--- src/Api/Serializers/ChatSerializer.php
+++ src/Api/Serializers/ChatSerializer.php
@@ -1,21 +1,15 @@
 <?php
-/*
- * This file is part of xelson/flarum-ext-chat
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
 
 namespace Xelson\Chat\Api\Serializers;
 
-use Flarum\User\User;
 use Flarum\Api\Serializer\AbstractSerializer;
-use Flarum\Settings\SettingsRepositoryInterface;
 use Flarum\Api\Serializer\UserSerializer;
+use Flarum\Settings\SettingsRepositoryInterface;
 
 class ChatSerializer extends AbstractSerializer
 {
     /**
+     * JSON:API resource type
      * @var string
      */
     protected $type = 'chats';
@@ -25,70 +19,45 @@ class ChatSerializer extends AbstractSerializer
      */
     protected $settings;
 
-    /**
-     * @var User
-     */
-    protected $actor;
-
-    /**
-     * @param PusherWrapper                 $pusher
-     */
-    public function __construct(
-        SettingsRepositoryInterface $settings
-    ) 
+    public function __construct(SettingsRepositoryInterface $settings)
     {
         $this->settings = $settings;
     }
 
     /**
-     * Get the default set of serialized attributes for a model.
-     *
-     * @param object|array $model
+     * @param object|array $chat
      * @return array
      */
-    protected function getDefaultAttributes($chat)
+    protected function getDefaultAttributes($chat): array
     {
         $attributes = $chat->getAttributes();
-        if($chat->created_at) $attributes['created_at'] = $this->formatDate($chat->created_at);
+        if ($chat->created_at) {
+            $attributes['created_at'] = $this->formatDate($chat->created_at);
+        }
 
         return $attributes;
     }
 
-    /**
-     * @return \Tobscure\JsonApi\Relationship
-     */
     protected function creator($chat)
     {
         return $this->hasOne($chat, UserSerializer::class);
     }
 
-    /**
-     * @return \Tobscure\JsonApi\Relationship
-     */
     protected function users($chat)
     {
         return $this->hasMany($chat, UserChatSerializer::class);
     }
 
-    /**
-     * @return \Tobscure\JsonApi\Relationship
-     */
     protected function messages($chat)
     {
         return $this->hasMany($chat, MessageSerializer::class);
     }
 
-    /**
-     * @return \Tobscure\JsonApi\Relationship
-     */
     protected function last_message($chat)
     {
         return $this->hasOne($chat, MessageSerializer::class);
     }
 
-    /**
-     * @return \Tobscure\JsonApi\Relationship
-     */
     protected function first_message($chat)
     {
         return $this->hasOne($chat, MessageSerializer::class);
