ad042a4f6ee09d5563d9ede5d068c8d7ec89421f
Lady-Byron <samantha-everett@outlook.com>
2025-11-07 16:31:56 -0500

    Update ChatState.js

--- js/src/forum/states/ChatState.js
+++ js/src/forum/states/ChatState.js
@@ -497,8 +497,8 @@ export default class ChatState {
           document.head.appendChild(s);
           self.executedScripts[scriptURL] = true;
         }
-      });
     })();
+  }
 
   handleAudioEmbeds(element, content) {
     const audioExts = ['.mp3', '.wav', '.ogg', '.m4a', '.aac', '.flac'];
