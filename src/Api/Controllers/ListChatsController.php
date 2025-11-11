08c443513e03eaa52cfc95e00e4e305e5b0231c4
Lady-Byron <samantha-everett@outlook.com>
2025-11-11 13:40:51 -0500

    Update ListChatsController.php

--- src/Api/Controllers/ListChatsController.php
+++ src/Api/Controllers/ListChatsController.php
@@ -1,15 +1,11 @@
 <?php
 /*
  * This file is part of xelson/flarum-ext-chat
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
  */
 
 namespace Xelson\Chat\Api\Controllers;
 
 use Xelson\Chat\Api\Serializers\ChatUserSerializer;
-use Xelson\Chat\Chat;
 use Xelson\Chat\ChatRepository;
 use Illuminate\Support\Arr;
 use Tobscure\JsonApi\Document;
@@ -19,51 +15,31 @@ use Flarum\Api\Controller\AbstractListController;
 
 class ListChatsController extends AbstractListController
 {
-    /**
-     * The serializer instance for this request.
-     *
-     * @var ChatUserSerializer
-     */
     public $serializer = ChatUserSerializer::class;
 
-    /**
-     * {@inheritdoc}
-     */
     public $include = [
-        'creator', 
-        'users', 
-        'last_message', 
+        'creator',
+        'users',
+        'last_message',
         'last_message.user',
         'first_message'
     ];
 
-    /**
-     * @var Dispatcher
-     */
     protected $bus;
+    protected $chats;
 
-    /**
-     * @param Dispatcher            $bus
-	 * @param ChatRepository        $chats
-     */
     public function __construct(Dispatcher $bus, ChatRepository $chats)
     {
-		$this->bus = $bus;
-		$this->chats = $chats;
-	}
-	
-    /**
-     * Get the data to be serialized and assigned to the response document.
-     *
-     * @param ServerRequestInterface $request
-     * @param Document               $document
-     * @return mixed
-     */
+        $this->bus = $bus;
+        $this->chats = $chats;
+    }
+
     protected function data(ServerRequestInterface $request, Document $document)
     {
         $actor = $request->getAttribute('actor');
         $include = $this->extractInclude($request);
+        $scope = Arr::get($request->getQueryParams(), 'scope'); // 管理员可用 '?scope=all'
 
-		return $this->chats->queryVisible($actor)->get()->load($include);
+        return $this->chats->queryVisible($actor, $scope)->get()->load($include);
     }
 }
