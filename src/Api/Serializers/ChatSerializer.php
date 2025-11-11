5a3ea92d878a77d98ea5ac3c3a1294eb75db3ade
Lady-Byron <samantha-everett@outlook.com>
2025-11-11 13:40:19 -0500

    Update ChatSerializer.php

--- src/Api/Serializers/ChatSerializer.php
+++ src/Api/Serializers/ChatSerializer.php
@@ -5,18 +5,12 @@ namespace Xelson\Chat\Api\Serializers;
 use Flarum\Api\Serializer\AbstractSerializer;
 use Flarum\Api\Serializer\UserSerializer;
 use Flarum\Settings\SettingsRepositoryInterface;
+use Xelson\Chat\Chat;
 
 class ChatSerializer extends AbstractSerializer
 {
-    /**
-     * JSON:API resource type
-     * @var string
-     */
     protected $type = 'chats';
 
-    /**
-     * @var SettingsRepositoryInterface
-     */
     protected $settings;
 
     public function __construct(SettingsRepositoryInterface $settings)
@@ -24,13 +18,10 @@ class ChatSerializer extends AbstractSerializer
         $this->settings = $settings;
     }
 
-    /**
-     * @param object|array $chat
-     * @return array
-     */
     protected function getDefaultAttributes($chat): array
     {
         $attributes = $chat->getAttributes();
+
         if ($chat->created_at) {
             $attributes['created_at'] = $this->formatDate($chat->created_at);
         }
@@ -53,13 +44,36 @@ class ChatSerializer extends AbstractSerializer
         return $this->hasMany($chat, MessageSerializer::class);
     }
 
+    /**
+     * 仅对“成员或管理员”暴露首末条，未加入公共频道/非成员不返回关系
+     */
     protected function last_message($chat)
     {
-        return $this->hasOne($chat, MessageSerializer::class);
+        if ($this->canSeeContent($chat)) {
+            return $this->hasOne($chat, MessageSerializer::class);
+        }
+        return null;
     }
 
     protected function first_message($chat)
     {
-        return $this->hasOne($chat, MessageSerializer::class);
+        if ($this->canSeeContent($chat)) {
+            return $this->hasOne($chat, MessageSerializer::class);
+        }
+        return null;
+    }
+
+    protected function canSeeContent(Chat $chat): bool
+    {
+        $actor = $this->actor;
+
+        if ($actor && $actor->isAdmin()) {
+            return true;
+        }
+
+        $pivot = $chat->getChatUser($actor);
+
+        return $pivot && !$pivot->removed_at;
     }
 }
+
